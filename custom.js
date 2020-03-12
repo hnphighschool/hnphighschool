3a2509e8f0d3ad29a6ea657699facd81ccdebf11
hnphighschool <62096553+hnphighschool@users.noreply.github.com>
2020-03-12 15:35:29 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,90 @@
+
+        var rollNumber= prompt("আপনার রোল নাম্বার প্রদান করুন");
+     
+        if(rollNumber==853384){
+            document.write("ছাত্রের নাম : আবিদ হোসেন রিওন ");
+            document.write("<br/>");
+            document.write("বিষয়:");
+            document.write("<br/>");
+            document.write("বাংলা: A+");
+            document.write("<br/>");
+            document.write("ইংরেজি: A+");
+            document.write("<br/>");
+            document.write("গণিত: A+");
+            document.write("<br/>");
+            document.write("আইসিটি: A+");
+            document.write("<br/>");
+            document.write("ধর্ম: A+");
+            document.write("<br/>");
+            document.write("সমাজবিজ্ঞান: A+");
+            document.write("<br/>");
+            document.write("পদার্থবিদ্যা: A+");
+            document.write("<br/>");
+            document.write("রসায়ন: A+");
+            document.write("<br/>");
+            document.write("জীববিদ্যা: A+");
+            document.write("<br/>");
+            document.write("উচ্চতর গণিত: A+");
+            document.write("<br/>");
+            document.write("<u>Grade:- GPA: 5:00</u>");
+        }
+        else if(rollNumber==8){
+            document.write("ছাত্রের নাম : আবিদ হোসেন রিওন ");
+            document.write("<br/>");
+            document.write("বিষয়:");
+            document.write("<br/>");
+            document.write("বাংলা: A+");
+            document.write("<br/>");
+            document.write("ইংরেজি: A+");
+            document.write("<br/>");
+            document.write("গণিত: A+");
+            document.write("<br/>");
+            document.write("আইসিটি: A+");
+            document.write("<br/>");
+            document.write("ধর্ম: A+");
+            document.write("<br/>");
+            document.write("সমাজবিজ্ঞান: A+");
+            document.write("<br/>");
+            document.write("পদার্থবিদ্যা: A+");
+            document.write("<br/>");
+            document.write("রসায়ন: A+");
+            document.write("<br/>");
+            document.write("জীববিদ্যা: A+");
+            document.write("<br/>");
+            document.write("উচ্চতর গণিত: A+");
+            document.write("<br/>");
+            document.write("<u>Grade:- GPA: 5:00</u>");
+        }
+
+        else if(rollNumber==1){
+            document.write("ছাত্রের নাম : আবিদ হোসেন রিওন ");
+            document.write("<br/>");
+            document.write("বিষয়:");
+            document.write("<br/>");
+            document.write("বাংলা: A+");
+            document.write("<br/>");
+            document.write("ইংরেজি: A+");
+            document.write("<br/>");
+            document.write("গণিত: A+");
+            document.write("<br/>");
+            document.write("আইসিটি: A+");
+            document.write("<br/>");
+            document.write("ধর্ম: A+");
+            document.write("<br/>");
+            document.write("সমাজবিজ্ঞান: A+");
+            document.write("<br/>");
+            document.write("পদার্থবিদ্যা: A+");
+            document.write("<br/>");
+            document.write("রসায়ন: A+");
+            document.write("<br/>");
+            document.write("জীববিদ্যা: A+");
+            document.write("<br/>");
+            document.write("উচ্চতর গণিত: A+");
+            document.write("<br/>");
+            document.write("<u>Grade:- GPA: 5:00</u>");
+         
+        }
+
+
+
+        else document.write("Fail! Reasult can't be found");
---
+++ main.js
@@ -0,0 +1,16 @@
+// const backtoTopButton = document.querySelector(".gotopbtn");
+// window.addEventListener("scroll",scrollFunction);
+// function scrollFunction(){
+//     if (window.pageYOffset>100){
+//         backtoTopButton.style.display="block";
+//     }
+//         else  {//backtotopbtn
+//         backtoTopButton.style.display="none"
+
+        
+//     }
+// }
+// backtoTopButton.addEventListener("click",backToTop);
+// function backToTop(){
+//     window.scroll(0,0);
+// }
